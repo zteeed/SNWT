c06ce9f4a7e6144da4163552ac37db0b9e8840e0
zteeed <zteeed@minet.net>
2018-10-26 16:53:05 +0000

    update title

--- website/index.php
+++ website/index.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>Miniport by HTML5 UP</title>
+    <title>Outil Respo Web</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="assets/css/main.css" />
