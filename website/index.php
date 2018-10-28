19145eb8e31225c5c1417c158e511c6215438124
zteeed <zteeed@minet.net>
2018-10-28 11:26:40 +0000

    example content set

--- website/index.php
+++ website/index.php
@@ -52,169 +52,136 @@
     <article id="adh" class="wrapper style2">
       <div class="container">
         <header>
-          <h2>Here's all the stuff I do.</h2>
-          <p>Odio turpis amet sed consequat eget posuere consequat.</p>
+          <h2>Adhérents</h2>
+          <p>...</p>
         </header>
         <div class="row aln-center">
-          <div class="col-4 col-6-medium col-12-small">
+          <div class="col-3 col-4-medium col-6-small">
             <section class="box style1">
               <span class="icon featured fa-comments-o"></span>
-              <h3>Consequat lorem</h3>
-              <p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
             </section>
           </div>
-          <div class="col-4 col-6-medium col-12-small">
+          <div class="col-3 col-4-medium col-6-small">
             <section class="box style1">
-              <span class="icon featured fa-camera-retro"></span>
-              <h3>Lorem dolor tempus</h3>
-              <p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+              <span class="icon featured fa-comments-o"></span>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
             </section>
           </div>
-          <div class="col-4 col-6-medium col-12-small">
+          <div class="col-3 col-4-medium col-6-small">
             <section class="box style1">
-              <span class="icon featured fa-thumbs-o-up"></span>
-              <h3>Feugiat posuere</h3>
-              <p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+              <span class="icon featured fa-comments-o"></span>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
+            </section>
+          </div>
+          <div class="col-3 col-4-medium col-6-small">
+            <section class="box style1">
+              <span class="icon featured fa-comments-o"></span>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
             </section>
           </div>
         </div>
-        <footer>
-          <a href="#hosting" class="button large scrolly">See some of my recent work</a>
-        </footer>
       </div>
     </article>
 
     <article id="hosting" class="wrapper style2">
       <div class="container">
         <header>
-          <h2>Here's all the stuff I do.</h2>
-          <p>Odio turpis amet sed consequat eget posuere consequat.</p>
+          <h2>Hosting</h2>
+          <p>...</p>
         </header>
         <div class="row aln-center">
-          <div class="col-4 col-6-medium col-12-small">
+          <div class="col-3 col-4-medium col-6-small">
             <section class="box style1">
               <span class="icon featured fa-comments-o"></span>
-              <h3>Consequat lorem</h3>
-              <p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
             </section>
           </div>
-          <div class="col-4 col-6-medium col-12-small">
+          <div class="col-3 col-4-medium col-6-small">
             <section class="box style1">
-              <span class="icon featured fa-camera-retro"></span>
-              <h3>Lorem dolor tempus</h3>
-              <p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+              <span class="icon featured fa-comments-o"></span>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
             </section>
           </div>
-          <div class="col-4 col-6-medium col-12-small">
+          <div class="col-3 col-4-medium col-6-small">
             <section class="box style1">
-              <span class="icon featured fa-thumbs-o-up"></span>
-              <h3>Feugiat posuere</h3>
-              <p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+              <span class="icon featured fa-comments-o"></span>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
+            </section>
+          </div>
+          <div class="col-3 col-4-medium col-6-small">
+            <section class="box style1">
+              <span class="icon featured fa-comments-o"></span>
+              <h3>Example Title</h3>
+              <p>Example text</p>
+              <ul>Example list
+                <li>Example item</li>
+                <li>Example item</li>
+                <li>Example item</li>
+              </ul>
             </section>
           </div>
         </div>
-        <footer>
-          <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
-          <a href="#portfolio" class="button large scrolly">See some of my recent work</a>
-        </footer>
       </div>
     </article>
 
-    <article id="dev" class="wrapper style3">
-      <div class="container">
-        <header>
-          <h2>Here’s some stuff I made recently.</h2>
-          <p>Proin odio consequat  sapien vestibulum consequat lorem dolore feugiat.</p>
-        </header>
-        <div class="row">
-          <div class="col-4 col-6-medium col-12-small">
-            <article class="box style2">
-              <a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-              <h3><a href="#">Magna feugiat</a></h3>
-              <p>Ornare nulla proin odio consequat.</p>
-            </article>
-          </div>
-          <div class="col-4 col-6-medium col-12-small">
-            <article class="box style2">
-              <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-              <h3><a href="#">Veroeros primis</a></h3>
-              <p>Ornare nulla proin odio consequat.</p>
-            </article>
-          </div>
-          <div class="col-4 col-6-medium col-12-small">
-            <article class="box style2">
-              <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-              <h3><a href="#">Lorem ipsum</a></h3>
-              <p>Ornare nulla proin odio consequat.</p>
-            </article>
-          </div>
-          <div class="col-4 col-6-medium col-12-small">
-            <article class="box style2">
-              <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-              <h3><a href="#">Tempus dolore</a></h3>
-              <p>Ornare nulla proin odio consequat.</p>
-            </article>
-          </div>
-          <div class="col-4 col-6-medium col-12-small">
-            <article class="box style2">
-              <a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-              <h3><a href="#">Feugiat aliquam</a></h3>
-              <p>Ornare nulla proin odio consequat.</p>
-            </article>
-          </div>
-          <div class="col-4 col-6-medium col-12-small">
-            <article class="box style2">
-              <a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
-              <h3><a href="#">Sed amet ornare</a></h3>
-              <p>Ornare nulla proin odio consequat.</p>
-            </article>
-          </div>
-        </div>
-        <footer>
-          <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
-          <a href="#contact" class="button large scrolly">Get in touch with me</a>
-        </footer>
-      </div>
-    </article>
-
-    <article id="prod" class="wrapper style4">
+    <article id="dev" class="wrapper style2">
       <div class="container medium">
         <header>
-          <h2>Have me make stuff for you.</h2>
-          <p>Ornare nulla proin odio consequat sapien vestibulum ipsum.</p>
+          <h2>Dev</h2>
+          <p>...</p>
         </header>
-        <div class="row">
-          <div class="col-12">
-            <form method="post" action="#">
-              <div class="row">
-                <div class="col-6 col-12-small">
-                  <input type="text" name="name" id="name" placeholder="Name" />
-                </div>
-                <div class="col-6 col-12-small">
-                  <input type="text" name="email" id="email" placeholder="Email" />
-                </div>
-                <div class="col-12">
-                  <input type="text" name="subject" id="subject" placeholder="Subject" />
-                </div>
-                <div class="col-12">
-                  <textarea name="message" id="message" placeholder="Message"></textarea>
-                </div>
-                <div class="col-12">
-                  <ul class="actions">
-                    <li><input type="submit" value="Send Message" /></li>
-                    <li><input type="reset" value="Clear Form" class="alt" /></li>
-                  </ul>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="col-12">
-            <ul class="social">
-              <li><a href="https://github.com/zteeed/SNWT" class="icon fa-github"><span class="label">Github</span></a></li>
-            </ul>
-            <hr />
-          </div>
-        </div>
+      </div>
+    </article>
+
+    <article id="prod" class="wrapper style2">
+      <div class="container medium">
+        <header>
+          <h2>Prod</h2>
+          <p>...</p>
+        </header>
       </div>
     </article>
 
