7c9b015a8de8fc7cc2511a63a6e4a784a96cb108
zteeed <zteeed@minet.net>
2018-10-29 22:53:17 +0000

    conn and create user feature

--- tools/setup.py
+++ tools/setup.py
@@ -1,48 +1,52 @@
 #! /usr/bin/python3
 
-import sys
 import os
+import re
+import sys
 import nmap
 import sqlalchemy
 from sqlalchemy import Table, Column, Integer, String, ForeignKey
-from sqlalchemy_utils import database_exists, create_database
+import sqlalchemy_utils
 
-class Database():
-
-    def __init__(self):
-        self.nmap = nmap.PortScanner()
-
-    def connect(user, password, db, host='localhost', port=5432):
-        '''Returns a connection and a metadata object'''
-        # postgresql://user:password@host:port/database
+def connect(user, db, password='', host='localhost', port=5432):
+    '''Returns a connection (and a metadata) object'''
+    # postgresql://user:password@host:port/database
+    if password:
         url = 'postgresql://{}:{}@{}:{}/{}'
         url = url.format(user, password, host, port, db)
-        #print(url)
+    else:
+        url = 'postgresql://{}@/{}'
+        url = url.format(user, db)
+    print("\nConnexion to: "+url)
+    try:
         con = sqlalchemy.create_engine(url, client_encoding='utf8')
-        meta = sqlalchemy.MetaData(bind=con, reflect=True)
-        return con, meta
-    
-    def create_db(user, password, db, host='localhost', port=5432):
-        url = 'postgresql://{}:{}@{}:{}/{}'
-        url = url.format(user, password, host, port, db)
-        con = sqlalchemy.create_engine(url)
-        if not database_exists(con.url):
-            create_database(con.url)
-        return
+        #meta = sqlalchemy.MetaData(bind=con, reflect=True)
+        return con 
+    except sqlalchemy.exc.OperationalError as exception:
+        exception = str(exception).split('\n')[0]
+        m = re.search('\(psycopg2.OperationalError\) FATAL:  (.*?)$', exception)
+        if m: 
+            print(m.group(1))
+            print("You might need to edit pg_hba.conf file.")
+            print("Don't forget to restart postgresql service after.")
+        sys.exit(0)
 
-    def create_tables(self):
-        return
+def create_user(username, password):
+    con = connect("postgres", "template1")
+    sql = 'CREATE USER {} WITH PASSWORD \'{}\''
+    sql = sql.format(username, password)
+    try:
+        con.execute(sql)
+        print ("Role {} has been successfully created.".format(username))
+    except sqlalchemy.exc.OperationalError as exception:
+        print (exception)
+        sys.exit(0)
+    except sqlalchemy.exc.ProgrammingError as exception:
+        print ("Role {} already exists, keep going...".format(username))
+    return
 
-url = 'postgresql://{}@{}:{}/{}'
-url = url.format("postgres", "localhost", 5432, "template1")
-con = sqlalchemy.create_engine(url)
-
-"""
-cat /ets/hosts
-192.168.104.11 hosting_db
 
 
-MXlf55DdYmURrHDlcbnYXKiGg2O
-
-create role respoweb with login;
-"""
+username = 'respoweb'
+password = 'MXlf55DdYmURrHDlcbnYXKiGg2O'
+create_user(username, password)
