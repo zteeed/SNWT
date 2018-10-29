48dfcb2c250e9727d6e2be057b7d12211efcea08
zteeed <zteeed@minet.net>
2018-10-29 15:34:37 +0000

    auto config and moving files to  var

--- install.sh
+++ install.sh
@@ -9,22 +9,43 @@ function ask_debian() {
   fi
 }
 
-function install_debian_stretch() {
-  sudo apt install apache2 php postgresql php-pgsql python3 python3-pip
-  sudo pip3 install -r requirements.txt
+function move() {
+  path=$1
+  rm -rf $path > /dev/null
+  sudo mkdir -p $path > /dev/null
+  cp -r {*,.git*} $path
+  echo
+  echo "Installation finished, you can go in $path"
+  exit 1
 }
 
-echo "-----------------------------------------------------"
-echo "----- Automatic Installation of the Environment -----"
-echo "-----------------------------------------------------"
-echo
-if lsb_release -a | grep -i "debian" > /dev/null; then
-  if lsb_release -a | grep -i "stretch" > /dev/null; then
-    echo "Debian stretch detected... OK!"; echo
-    install_debian_stretch
+function install_debian_stretch() {
+  path="/var/www/html/"
+  sudo apt install apache2 php postgresql php-pgsql python3 python3-pip
+  sudo pip3 install -r requirements.txt
+  read -p "Deploy all files with deleting all in $path ? (y/N) " -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then 
+    exit 1
+  else
+    move $path
+  fi
+}
+
+function main() {
+  echo "-----------------------------------------------------"
+  echo "----- Automatic Installation of the Environment -----"
+  echo "-----------------------------------------------------"
+  echo
+  if lsb_release -a | grep -i "debian" > /dev/null; then
+    if lsb_release -a | grep -i "stretch" > /dev/null; then
+      echo "Debian stretch detected... OK!"; echo
+      install_debian_stretch
+    else
+      ask_debian
+    fi
   else
     ask_debian
   fi
-else
-  ask_debian
-fi
+}
+
+main
