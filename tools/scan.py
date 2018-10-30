a680929af6ba55d3cb4beb418cf89e2a66f4cc57
zteeed <zteeed@minet.net>
2018-10-30 06:50:22 +0000

    init scan script

--- tools/scan.py
+++ tools/scan.py
@@ -1,48 +1,28 @@
 #! /usr/bin/python3
 
+import re
 import sys
-import os
-import nmap
 import sqlalchemy
 from sqlalchemy import Table, Column, Integer, String, ForeignKey
-from sqlalchemy_utils import database_exists, create_database
+import sqlalchemy_utils
+import nmap
+import setup
 
-class Database():
-
-    def __init__(self):
-        self.nmap = nmap.PortScanner()
-
-    def connect(user, password, db, host='localhost', port=5432):
-        '''Returns a connection and a metadata object'''
-        # postgresql://user:password@host:port/database
-        url = 'postgresql://{}:{}@{}:{}/{}'
-        url = url.format(user, password, host, port, db)
-        #print(url)
-        con = sqlalchemy.create_engine(url, client_encoding='utf8')
-        meta = sqlalchemy.MetaData(bind=con, reflect=True)
-        return con, meta
-    
-    def create_db(user, password, db, host='localhost', port=5432):
-        url = 'postgresql://{}:{}@{}:{}/{}'
-        url = url.format(user, password, host, port, db)
-        con = sqlalchemy.create_engine(url)
-        if not database_exists(con.url):
-            create_database(con.url)
-        return
-
-    def create_tables(self):
-        return
-
-url = 'postgresql://{}@{}:{}/{}'
-url = url.format("postgres", "localhost", 5432, "template1")
-con = sqlalchemy.create_engine(url)
-
-"""
-cat /ets/hosts
-192.168.104.11 hosting_db
-
-
-MXlf55DdYmURrHDlcbnYXKiGg2O
-
-create role respoweb with login;
-"""
+def start():
+    nm = nmap.PortScanner()
+    result = nm.scan(hosts="157.159.45.91", arguments='-T5')
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (host, nm[host].hostname()))
+        print('State : %s' % nm[host].state())
+        for proto in nm[host].all_protocols():
+            print('----------')
+            print('Protocol : %s' % proto)
+            lport = nm[host][proto].keys()
+            for port in sorted(lport):
+                print ('port : %s\tstate : %s\tname : %s' 
+                       % (port, 
+                          nm[host][proto][port]['state'], 
+                          nm[host][proto][port]['name'] )
+                      )
+    print (nm.csv())
